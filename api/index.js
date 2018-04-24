1b2e042ebd4292e7334b76c4f303b38af87f15c5
Huang Guolin <AlvinHuang@GRAPECITY.NET>
2018-04-24 10:57:15 +0800

    add cross origin allowed

--- api/index.js
+++ api/index.js
@@ -16,11 +16,19 @@ function registerApi (app, api) {
     const allowedMethods = Object.keys(api.methods);
 
     app.all(path, function (req, res, next) {
+        // allow cross origin
+        if (req.method === 'OPTIONS') {
+            res.header("Access-Control-Allow-Methods", allowedMethods.map(e => e.toUpperCase()).join());
+            res.send();
+            return;
+        }
+
         // handle not allow method
         const needle = allowedMethods.indexOf(req.method.toLowerCase());
         if (needle === -1) {
             res.append('Allow', allowedMethods.map(e => e.toUpperCase()).join());
             res.status(405).send();
+            return;
         }
 
         // pre-proc
--- app.js
+++ app.js
@@ -6,22 +6,31 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes');
 var registerAllApi = require('./api');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
 
+// basic configure
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+// root router configure
 app.use('/', indexRouter);
+
+// allow cross origin
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+    next();
+});
 
 // register RESTful api
 registerAllApi(app);
